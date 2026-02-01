fb55884905eceb037263dd11c917003db100f6c6
Angelos Drossos <develangel@mail.drossos.de>
2026-02-01 06:44:12 +0000

    feat(cache): stabilize cache order
    Use IndexSet from indexmap crate for caching.

--- types/src/server.rs
+++ types/src/server.rs
@@ -1,8 +1,8 @@
 use crate::Task;
 
+use indexmap::IndexSet;
 use uuid::Uuid;
 
-use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
 use std::ops::{Deref, DerefMut};
 
@@ -65,15 +65,15 @@ pub trait TaskList {
 }
 
 #[derive(Debug)]
-pub struct TaskCache(HashSet<CachedTask>);
+pub struct TaskCache(IndexSet<CachedTask>);
 
 impl<'a> Default for TaskCache {
     fn default() -> Self {
-        let mut set = HashSet::with_capacity(10);
+        let mut set = IndexSet::with_capacity(10);
         let list = vec![
-            Task::default().summary("Task A.1"),
-            Task::default().summary("Task B.2"),
-            Task::default().summary("Task C.3"),
+            Task::new("Task A.1"),
+            Task::new("Task B.2"),
+            Task::new("Task C.3"),
         ];
         list.into_iter().for_each(|task| {
             set.insert(CachedTask(task));
@@ -93,6 +93,6 @@ impl TaskList for TaskCache {
 
     fn remove<T: Into<Uuid>>(&mut self, id: T) -> bool {
         let id = id.into();
-        self.0.remove(&CachedTask(Task::with_id(id)))
+        self.0.shift_remove(&CachedTask(Task::with_id(id)))
     }
 }
