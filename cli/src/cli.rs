d3b5f5a9d5303dc8be8003a20ee09f484b76188f
Angelos Drossos <develangel@mail.drossos.de>
2026-03-07 02:50:41 +0000

    feat(cli): support env settings

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -31,43 +31,48 @@ pub enum Commands {
     },
     /// List all tasks
     List {
-        #[clap(flatten)]
-        server: ServerArgs,
         /// printed in JSON
         #[clap(short, long, default_value_t = false)]
         json: bool,
         /// pretty-printed JSON
         #[clap(short, long, default_value_t = false, requires("json"))]
         pretty: bool,
+        /// server options
+        #[clap(flatten)]
+        server: ServerArgs,
     },
     /// Add a new task
     Add {
-        #[clap(flatten)]
-        server: ServerArgs,
         /// Task summary
         #[clap(short, long)]
         summary: String,
         /// Task details as JSON string (see schema)
         #[clap(short, long)]
         details: Option<String>,
+        /// server options
+        #[clap(flatten)]
+        server: ServerArgs,
     },
     /// Complete task (or uncomplete it)
     Complete {
-        #[clap(flatten)]
-        server: ServerArgs,
         /// Task ID
         #[clap(short, long)]
         id: Uuid,
         /// New task status
         #[clap(short, long, default_value_t = TaskStatus::Done)]
         status: TaskStatus,
+        /// server options
+        #[clap(flatten)]
+        server: ServerArgs,
     },
 }
 
 #[derive(Debug, Args)]
+#[clap(next_help_heading = "Server Options")]
+#[clap(after_help = "The Server Options are not relevant for AI assistants.")]
 pub struct ServerArgs {
     /// Sets the server address to connect to.
-    #[clap(long = "server", default_value_t = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9000))]
+    #[clap(long = "server", env = "KID_SERVER_ADDR", default_value_t = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9000))]
     pub addr: SocketAddr,
 }
 
